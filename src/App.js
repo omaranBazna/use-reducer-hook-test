a02425a1aada244ed610991ee2a0ada72bdc17e1
omaran bazna <omran4d@gmail.com>
2022-08-01 18:30:54 -0400

    add the input to the main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Input from "./components/Input";
 function App() {
   return (
     <div className="app">
-      <div className="container"></div>
+      <div className="container">
+        <Input></Input>
+      </div>
     </div>
   );
 }

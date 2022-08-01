035a46b49f93161cdaada035d2dbf4e049cf9e50
omaran bazna <omran4d@gmail.com>
2022-08-01 18:54:16 -0400

    define the initial state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import Item from "./components/Item";
 import { v4 as uuid } from "uuid";
 import { useReducer } from "react";
 function App() {
-  const items = [
+
+  const reducer=(state,action)=>{
+     switch (action.type){
+     case "add":
+       return{
+        
+       }
+     case "remove":
+
+     default:
+
+     }
+  }
+  const initState={
+ items : [
     { name: "milk", id: uuid() },
     { name: "chocolate", id: uuid() },
     { name: "banana", id: uuid() },
   ];
+}
   return (
     <div className="app">
       <div className="container">

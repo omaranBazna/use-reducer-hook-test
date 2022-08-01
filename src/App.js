88056982a93ef72b16f50ee54b509dd2acab99b9
omaran bazna <omran4d@gmail.com>
2022-08-01 18:33:11 -0400

    define the list of items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Input from "./components/Input";
+import Item from "./components/Item";
 function App() {
+  const items = ["milk", "chocalete", "banana"];
   return (
     <div className="app">
       <div className="container">
